19df19789950280badd7c8cf66477ec0679b2f8b
Omrikibubu <omrizitman@gmail.com>
2023-12-05 22:56:23 +0200

    ////

--- drf_oz_product/oz_product_proj/urls.py
+++ drf_oz_product/oz_product_proj/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('oz_product.urls')),
+    path('', include('oz_product.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
