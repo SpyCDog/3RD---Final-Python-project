2cb897c745724445b3a0547e955520fcc245cbf7
Omrikibubu <omrizitman@gmail.com>
2023-12-07 19:19:57 +0200

    lottie added

--- react-oz-product/src/components/Navbar.js
+++ react-oz-product/src/components/Navbar.js
@@ -2,28 +2,57 @@ import { useState, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
 import { UserContext } from './UserContext';
+import Lottie from 'lottie-react';
+import morningAnimationData from './styles/lottie/morning.json';
+import afternoonAnimationData from './styles/lottie/noon.json';
+import eveningAnimationData from './styles/lottie/evening.json';
 
-function getTimeBasedGreeting(username) {
-  const hour = new Date().getHours(); // Get the current hour
-  let greeting = "Welcome";
 
-  if (hour < 12) {
-      greeting = "Good morning";
-  } else if (hour < 18) {
-      greeting = "Good afternoon";
-  } else {
-      greeting = "Good evening";
-  }
+// function getTimeBasedGreeting(username) {
+//   const hour = new Date().getHours(); // Get the current hour
+//   let greeting = "Welcome";
 
-  return `${greeting} ${username}`;
-}
+//   if (hour < 12) {
+//       greeting = "Good morning";
+//   } else if (hour < 18) {
+//       greeting = "Good afternoon";
+//   } else {
+//       greeting = "Good evening";
+//   }
+  
+
+//   return `${greeting} ${username}`;
+// }
 
 function Navbar({ categories, clickButton, searchProduct }) {
   const [searchText, setSearchText] = useState(""); // this is the value of the search field
   const location = useLocation();
   const { user, logout } = useContext(UserContext);
+  
+  const getGreetingAnimation = (username) => {
+    const hour = new Date().getHours(); // Get the current hour
+    let greetingAnimationData = morningAnimationData; // Default to morning animation
 
+    if (hour < 12) {
+      greetingAnimationData = morningAnimationData;
+    } else if (hour < 18) {
+      greetingAnimationData = afternoonAnimationData;
+    } else {
+      greetingAnimationData = eveningAnimationData;
+    }
 
+    const defaultOptions = {
+      loop: true,
+      autoplay: true, 
+      animationData: greetingAnimationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+    return (
+      <Lottie options={defaultOptions} height={120} width={120} />
+      );
+    };
   return (
     <>
       <ul className="nav my-4">
@@ -62,7 +91,7 @@ function Navbar({ categories, clickButton, searchProduct }) {
           <li className="nav-item">
             {user ? (
               <>
-                <span><span>{getTimeBasedGreeting(user.username)}</span></span>
+                <span><span>{getGreetingAnimation(user.username)}</span></span>
                 <button className="mx-1 btn btn-danger" onClick={logout}>Logout</button>
               </>
               ) : (
