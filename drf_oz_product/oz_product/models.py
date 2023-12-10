2bbf71d5045eda6c3480f1813c55859582418691
Omrikibubu <omrizitman@gmail.com>
2023-12-10 21:17:15 +0200

    added str to models

--- drf_oz_product/oz_product/models.py
+++ drf_oz_product/oz_product/models.py
@@ -20,12 +20,16 @@ class Product(models.Model):
 class Cart(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart')
     created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.user, self.created_at}'
     
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-
+    
+    def __str__(self):
+        return f'{self.product, self.quantity}'
 
 
 class Category(models.Model):  
