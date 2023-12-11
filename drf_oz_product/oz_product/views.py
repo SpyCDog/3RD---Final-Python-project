8e1a50bd6c74b439670d1bc9d2530431b3787228
Omrikibubu <omrizitman@gmail.com>
2023-12-11 14:46:35 +0200

    register views.py

--- drf_oz_product/oz_product/views.py
+++ drf_oz_product/oz_product/views.py
@@ -152,24 +152,17 @@ def register(request):
         password = request.data.get('password')
         email = request.data.get('email')
         
+        MyUser.objects.get("email")
         # Validate the data, for instance:
-        if not username or not password or not email:
-            return Response({'error': 'Username, password, and email are required.'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if User.objects.filter(username=username).exists():
-            return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if User.objects.filter(email=email).exists():
+        if  not email:
             return Response({'error': 'Email already in use.'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Create the user
-        user = MyUser.objects.create_user(username=username, email=email, password=password)
-        user.save()
-        
-        # You can now do any post-registration logic like sending a confirmation email, etc.
+        else:
+            # Create the user
+            new_user = MyUser.objects.create_user(username=username, email=email, password=password)
+            new_user.save()
 
         # Return a successful response
-        return Response(f"{user} just registered!!!",{'message': 'User registered successfully.'}, status=status.HTTP_201_CREATED)
+        return Response(f"{new_user} just registered!!!",{'message': 'User registered successfully.'}, status=status.HTTP_201_CREATED)
     
     except Exception as e:
         # Return a response with an error message
