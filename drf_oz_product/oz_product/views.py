c67c99021d7fe15a936ea39f06cceae3ffb9fcfb
Omrikibubu <omrizitman@gmail.com>
2023-12-12 13:14:16 +0200

    sss

--- drf_oz_product/oz_product/views.py
+++ drf_oz_product/oz_product/views.py
@@ -106,10 +106,10 @@ def add_to_cart(request):
                 cart_item.quantity = int(quantity)
             cart_item.save()
             cart.save()
-            # cart_item_serializer = CartItemSerializer(cart_item)
-            cart_serializer = CartSerializer(cart)
+            cart_item_serializer = CartItemSerializer(cart_item)
+            # cart_serializer = CartSerializer(cart)
             # serailizers = [cart_item_serializer, cart_serializer]
-            return Response(cart_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(cart_item_serializer.data, status=status.HTTP_201_CREATED)
         except Product.DoesNotExist:
             return Response({'Product not found.(backend - add_to_cart)'}, status=status.HTTP_404_NOT_FOUND)
         
